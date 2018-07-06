81371412a34a7eada4475bf79b93dd6a52835e0b
Carl Colglazier <carl@carlcolglazier.com>
2018-07-06 15:12:28 -0400

    Use only Python functions (no SQL).

--- kaskeda.py
+++ kaskeda.py
@@ -1,37 +1,14 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import inspect
 import json
 import bz2
+import pandas as pd
+from collections import Counter
 
-from sqlalchemy import func, desc
-
-# TODO: Connect this to a persistent database.
-engine = create_engine('sqlite:///:memory:')#, echo=True)
-
-Base = declarative_base()
-Session = sessionmaker(bind=engine)
-inspector = inspect(engine)
-
-class Submission(Base):
-    __tablename__ = 'submissions'
-
-    author = Column(String)
-    created_utc = Column(Integer)
-    domain = Column(String)
-    id = Column(String, primary_key=True)
-    num_comments = Column(Integer)
-    score = Column(Integer)
-    subreddit = Column(String)
-    title = Column(String)
-    url = Column(String)
-
-session = Session()
-session.commit()
-
-Base.metadata.create_all(bind=engine)
+keys = [
+    'author', 'created_utc',
+    'domain', 'id', 'num_comments',
+    'score', 'subreddit', 'title',
+    'url'
+]
 
 def open_submissions(file):
     submissions = []
@@ -40,41 +17,24 @@ def open_submissions(file):
     with bz2.open(file) as f:
         for line in f:
             i += 1
-            if i > 100000:
+            if i > 250000:
                 break
-            submissions.append(line)
+            try:
+                l = json.loads(line)
+                submissions.append({k: l[k] for k in keys})
+            except:
+                # TODO: Why are some submissions not working?
+                # Not a huge deal: we can ignore them for now,
+                # but this would be good to fix later.
+                pass
     return submissions
 
 # TODO: Get this from an argument.
-subs = open_submissions("./data/RS_2016-10.bz2")
-print(len(subs))
+submissions = open_submissions("./data/RS_2016-10.bz2")
+print(len(submissions))
 
-submissions = []
-keys = [x["name"] for x in inspector.get_columns('submissions')]
-for s in subs:
-    try:
-        x1 = json.loads(s)
-        x2 = {k: x1[k] for k in keys}
-        submissions.append(x2)
-    except:
-        # TODO: Why are some submissions not working?
-        # Not a huge deal: we can ignore them for now,
-        # but this would be good to fix later.
-        print(s)
-
-session.bulk_insert_mappings(Submission, submissions)
-session.commit()
-
-print(session.query(func.count('*')).select_from(Submission).scalar())
-print(session.query(Submission).filter(Submission.domain.like('rt.com')).count())
-print(session.query(Submission).filter(Submission.domain.like('economist.com')).count())
-print(session.query(Submission).filter(Submission.domain.like('aljazeera.com')).count())
-
-# Print out the top domains.
-print(
-    session.query(Submission.domain, func.count(Submission.domain)
-    ).group_by(Submission.domain
-    ).order_by('count_1 desc'
-    ).limit(100
-    ).all()
-)
+t = pd.DataFrame(submissions)
+print(t[t["domain"] == 'rt.com']["domain"].count())
+print(t[t["domain"] == 'economist.com']["domain"].count())
+print(t[t["domain"] == 'aljazeera.com']["domain"].count())
+print(t["domain"].value_counts()[0:100])
