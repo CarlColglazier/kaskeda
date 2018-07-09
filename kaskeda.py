0871a711ffb3612efe52f8ae4b071c16307db301
Carl Colglazier <carl@carlcolglazier.com>
2018-07-09 14:07:22 -0400

    Use generators for lines.

--- kaskeda.py
+++ kaskeda.py
@@ -27,41 +27,26 @@ def open_compressed(path):
         return lzma.open(path)
     return open(path)
     
-def open_submissions(file):
-    submissions = []
-    # This is over eight million lines!
-    with open_compressed(file) as f:
-        for line in f:
-            try:
-                l = json.loads(line)
-                submissions.append({k: l[k] for k in keys})
-            except:
-                # TODO: Why are some submissions not working?
-                # Not a huge deal: we can ignore them for now,
-                # but this would be good to fix later.
-                pass
-    return submissions
-
-def test_read(path):
+def count_rt(path):
     subs = []
-    with open_compressed(path) as f:
-        i = 0
-        for line in f:
-            #i += 1
-            try:
-                if 'domain":"rt.com' in str(line):
-                    l = json.loads(line)
-                    #if l['domain'] == 'rt.com':
-                    subs.append(l['subreddit'])
-            except:
-                print(path, line)
-                pass
-            #if i > 1000000:
-            #    break
+    # Adjust this to just take a sample.
+    i = 100000000
+    for line in itertools.islice(open_compressed(path), i):
+        try:
+            if 'domain":"rt.com' in str(line):
+                l = json.loads(line)
+                subs.append(l["subreddit"])
+        except:
+            print(path, line)
+            # TODO: Why are some submissions not working?
+            # Not a huge deal: we can ignore them for now,
+            # but this would be good to fix later.
+            pass
     return subs
 
 if __name__ == '__main__':
-    path = './data/'
+    # TODO: Don't hardcode this.
+    path = '/scratch/ccolglaz/data/'
     files = []
     for file in os.listdir(path):
         # Use join to get full file path.
@@ -70,7 +55,6 @@ if __name__ == '__main__':
         files.append((size, location))
     files.sort(key=lambda s: s[0], reverse=True)
     files = [x[1] for x in files]
-    
     p = Pool(64)
-    subs = p.map(test_read, files)
+    subs = p.map(count_rt, files)
     print(Counter(itertools.chain(*subs)).most_common(100))
