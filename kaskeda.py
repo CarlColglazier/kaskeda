d8d480d8c07b38c50b2026eae04cfbdfbde4b570
Carl Colglazier <carl@carlcolglazier.com>
2018-07-10 13:52:25 -0400

    Use objects instead of classes for process list (#1).

--- kaskeda.py
+++ kaskeda.py
@@ -1,5 +1,4 @@
 import json
-import pandas as pd
 from collections import Counter
 import itertools
 import os
@@ -9,13 +8,6 @@ import bz2
 # Multiprocessing to run in parallel
 from multiprocessing import Pool
 from functools import partial
-
-keys = [
-    'author', 'created_utc',
-    'domain', 'id', 'num_comments',
-    'score', 'subreddit', 'title',
-    'url'
-]
 
 def open_compressed(path):
     """
@@ -41,22 +33,55 @@ class RedditProcessor:
         except:
             pass
 
-class RTCounter(RedditProcessor):
-    def filter(self, line):
-        #return True
-        return 'domain":"rt.com' in str(line)
+class RedditCounter(RedditProcessor):
+    def __init__(self):
+        self.store = Counter()
 
-class BBCCounter(RedditProcessor):
+    def process(self, obj):
+        try:
+            self.store[obj["subreddit"]] += obj["score"]
+        except:
+            pass
+        
+
+class DomainCounter(RedditCounter):
+    def __init__(self, domain):
+        super().__init__() 
+        self.string = 'domain":"{}"'.format(domain)
+
     def filter(self, line):
-        #return True
-        return 'domain":"bbc.com' in str(line)
+        return self.string in str(line)
+
+class SubredditDomainCounter(DomainCounter):
+    def __init__(self, sub):
+        self.store = Counter()
+        self.string = 'subreddit":"{}"'.format(sub)
+
+    def process(self, obj):
+        try:
+            self.store[obj["domain"]] += 1
+        except:
+            pass
+
+class StringSearchCounter(RedditCounter):
+    def __init__(self, word):
+        super().__init__()
+        self.string = word
+
+    def filter(self, line):
+        return self.string in str(line)
+
+    def process(self, obj):
+        try:
+            self.store[obj["subreddit"]] += obj["score"]
+        except:
+            pass
 
 class RedditScraper:
     process_list = []
     def __init__(self, processes=1, path='./data'):
         self._pool = Pool(processes)
         self.path = path
-        #self._process_list = []
         self._files = self._get_files()
 
     def _get_files(self):
@@ -74,7 +99,7 @@ class RedditScraper:
         RedditScraper.process_list.append(p)
 
     def _run(f, processes):
-        classes = [x() for x in processes]
+        classes = processes #[x() for x in processes]
         i = 10000000
         for line in itertools.islice(open_compressed(f), i):
             for c in classes:
@@ -92,16 +117,19 @@ class RedditScraper:
         )
         # flatten
         results = [list() for _ in range(len(RedditScraper.process_list))]
-        for p_results in p:
-            for i, r in enumerate(p_results):
-                results[i] += r.store
+        for i in range(len(results)):
+            # TODO: Should this be a function of RedditProcessor instead?
+            # That would allow different types of objects to be used.
+            results[i] = sum([x[i].store for x in p], Counter())
         return results
 
 if __name__ == '__main__':
     scraper = RedditScraper(processes=64, path='/scratch/ccolglaz/data/')
-    scraper.add_process(RTCounter)
-    scraper.add_process(BBCCounter)
+    scraper.add_process(DomainCounter("rt.com"))
+    scraper.add_process(DomainCounter("bbc.com"))
+    scraper.add_process(DomainCounter("aljazeera.com"))
+    #scraper.add_process(StringSearchCounter("Neil Gorsuch"))
     results = scraper.run()
-    print(Counter(results[0]).most_common(10))
-    print(Counter(results[1]).most_common(10))
-    
+    for g in range(len(results)):
+        for i, (domain, count) in enumerate(results[g].most_common(25)):
+            print("{}. {} ({})".format(i + 1, domain, count))
